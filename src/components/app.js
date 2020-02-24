fb89c6a7e1a01bb3fa227b94e5c9f8fd73a1271e
Safarov <elkhan.safarov10@yandex.com>
2020-02-24 23:38:48 +0500

    Добавление задач в список

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,43 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import './app.css';
 
-export default class App extends Component {
+class App extends Component {  
+    learn = (event) => {
+        event.target.classList.toggle('Completed-item');
+        event.target.classList.toggle('Item');
+    }
+    addValue = (event) => {
+        event.preventDefault();
+        this.props.onAddValue(this.inoutValue.value);
+        this.inoutValue.value = '';
+    }
     render() {
         return (
             <div className="todo-form">
                 <form className="Form">
-                    <div className="tasks">
-                        <p className="Completed-item">Learn React</p>
-                        <p className="Item">Redux is awesme</p>
+                    <div className="tasks" onClick={this.learn} ref={this.myRef}>
+                        {
+                            this.props.testStore.map((item, index) => {
+                                return <p key={index} className="Item">{item}</p>
+                            })
+                        }
                     </div>
-                    <input type="text" className="Input-block" placeholder="Enter a new todo item"/>
-                    <button className="Hide-btn">Hide completed</button>
-                    <button className="Add-btn">Add new</button>
+                    <input type="text" className="Input-block" placeholder="Enter a new todo item" ref={(input) => {this.inoutValue = input}}/>
+                    <button className="Hide-btn" onClick={this.hideCompleted}>Hide completed</button>
+                    <button className="Add-btn" onClick={this.addValue}>Add new</button>
                 </form>
             </div>
         )
     }
 }
+export default connect(
+    state => ({
+        testStore: state
+    }),
+    dispatch => ({
+        onAddValue: (valueName) => {
+            dispatch({type: 'ADD_VALUE', payload: valueName})
+        }
+    })
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const initialState = ['Learn React', 'Redux is awesme'];
+function reducer(state = initialState, action) {
+    if(action.type === 'ADD_VALUE') {
+        return [
+            ...state,
+            action.payload
+        ]
+    }
+    return state;
+}
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'))
